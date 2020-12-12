e02c3ba6651a09ae1eb54679e8d6f58b2fb563fd
vince <vince20015@gmail.com>
2020-12-12 22:37:48 +0100

    begin of gridbased movement

--- Godot/Bikeman/PlayerGridMovement.gd
+++ Godot/Bikeman/PlayerGridMovement.gd
@@ -1,14 +1,12 @@
 extends Area2D
 
-export var speed = 7
+export var speed = 8
 
+const tile_size = 32
 onready var tween = $Tween
 onready var ray = $RayCast2D
 
-var moving = false
-var temp_dir
-var tile_size = 32
-var inputs = {
+var directions = {
 	"RIGHT": Vector2.RIGHT,
 	"LEFT": Vector2.LEFT,
 	"UP": Vector2.UP,
@@ -16,38 +14,33 @@ var inputs = {
 	"STOP": Vector2.ZERO,
 }
 
-var newDir
+var newdir = Vector2.LEFT
+var dir = directions.LEFT
+var noCol = true
+
 
 func _ready():
 	position = position.snapped(Vector2.ONE * tile_size)
 	position += Vector2.ONE * tile_size/2
-	newDir
 	
-func _unhandled_input(event):
-	for dir in inputs.keys():
-		if event.is_action(dir):
-			newDir = dir
-
-
 func _process(delta):
 	if tween.is_active():
 		return
-	for dir in inputs.keys():
-		move(dir)
-		print("This is from VSCode")
+	if newdir != dir:
+		ray.cast_to = directions[newdir] * tile_size
+		ray.force_raycast_update()
+		if !ray.is_colliding():
+			dir = newdir
+			move_tween(dir)
+		else:
+			move_tween(dir)
 	
-	
-func move(dir):
-	ray.cast_to = inputs[dir] * tile_size
-	ray.force_raycast_update()
-	if !ray.is_colliding():
-		move_tween(dir)
-	else:
-		dir = "STOP"
+	if Input.is_action_pressed("DOWN"):
+		
 		
 func move_tween(dir):
 	tween.interpolate_property(self, "position",
-	position, position + inputs[dir] * tile_size,
+	position, position + directions[dir] * tile_size,
 		1.0/speed, Tween.TRANS_LINEAR)
 	tween.start()
 
